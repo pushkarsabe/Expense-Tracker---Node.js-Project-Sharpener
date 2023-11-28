5d34c0aace002f52fa55c8ef4f2484fd00dd5a92
Pushkar Sabe <83827883+pushkarsabe@users.noreply.github.com>
2023-11-28 11:46:09 +0530

    Question 4 =  Creating Login Screen Frontend

--- user.js
+++ user.js
@@ -1,36 +1,47 @@
 //importing error to handle it
-const { Sequelize } = require('sequelize');
-const { User } = require('../models/user');
+const User = require('../models/user');
 
 //to post the records to database
 exports.postAddUser = async (req, res, next) => {
-    try {
-
-        const name = req.body.name;
-        const email = req.body.email;
-        const password = req.body.password;
-        console.log('name = ' + name);
-        console.log('email = ' + email);
-        console.log('password = ' + password);
-
-        const newUser = await User.create({
-            name: name,
-            email: email,
-            password: password,
-        });
-        //will send json response back to the client
-        res.status(201).json({ newUserDetails: newUser });
+    //post always uses bosy
+    const name = req.body.name;
+    const email = req.body.email;
+    const password = req.body.password;
+    console.log('name = ' + name);
+    console.log('email = ' + email);
+    console.log('password = ' + password);
+    if (name == '' || name == undefined || email == '' || email == undefined || password == '' || password == undefined) {
+        return res.status(400).json({ err: 'Input fields are empty' })
     }
-    catch (err) {
-        //this is to handle duplicate email error and you can handle particular sequelize error
-        if (err === '') {
-            res.status(400).json({
-                error: 'duplicate email',
-            })
-        }
-        console.log('Post user is failing' + JSON.stringify(err));
-        res.status(500).json({
-            error: err,
-        })
+
+    const newUser = await User.create({
+        name: name,
+        email: email,
+        password: password,
+    });
+    //will send json response back to the client
+    res.status(201).json({ newUserDetails: newUser });
+}
+
+//to get the records from db
+exports.postSignUser = async (req, res, next) => {
+    //get always uses param
+    const email = req.body.email;
+    const password = req.body.password;
+    console.log('email = ' + email);
+    console.log('password = ' + password);
+
+    if (email == '' || email == undefined) {
+        return res.status(400).json({ err: 'email required' });
     }
+
+    const userData = await User.findAll({
+        where: { email: email }
+    })
+
+    if (userData.length == 0) {
+        return res.status(400).json({ err: 'User not found' });
+    }
+    //will send json response back to the client
+    res.status(201).json({ userDetails: userData });
 }

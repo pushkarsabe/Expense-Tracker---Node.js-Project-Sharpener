85a5d3c73608d563aa04f060e68a84d7210c3861
Pushkar Sabe <83827883+pushkarsabe@users.noreply.github.com>
2023-12-01 20:08:17 +0530

    Question 12 = Final Leaderboard Optimisation

--- expense.js
+++ expense.js
@@ -1,16 +1,72 @@
-const express = require('express');
+const Expense = require('../models/expense');
+const User = require('../models/user');
 
-const userAuthorization = require('../middlware/auth');
+exports.postAddExpense = async (req, res, next) => {
+    //post always uses body
+    try {
+        const money = req.body.money;
+        const description = req.body.description;
+        const options = req.body.options;
+        console.log('money = ' + money);
+        console.log('description = ' + description);
+        console.log('options = ' + options);
+        console.log('userid = ' + req.user.id);
+        if (money == '' || money == undefined || description == '' || description == undefined || options == '' || options == undefined) {
+            return res.status(400).json({ message: 'Input fields are empty' })
+        }
 
-const router = express.Router();
+        const expenseData = await Expense.create({
+            money: money,
+            description: description,
+            options: options,
+            userid: req.user.id
+        });
+        //to keep the total money updated spend by the user every time new expense is added 
+        const totalExpense = Number(req.user.totalExpenses) + Number(money);
+        console.log('req.user objet= ' + JSON.stringify(req.user));
+        console.log('totalExpense = ' + totalExpense);
+        console.log('userid = ' + req.user.id);
+        //this returns promise
+        const [updatedRowsCount] = await User.update({
+            totalExpenses: totalExpense
+        },
+            {
+                where: {
+                    id: req.user.id
+                },
 
-const expenseController = require('../controllers/expense');
+            });
 
-router.post('/add-expense', userAuthorization.authenticate, expenseController.postAddExpense);
+        console.log('Updated rows count:', updatedRowsCount);
 
-router.get('/get-expense', userAuthorization.authenticate, expenseController.getExpense);
+        res.status(200).json({ newExpenseData: expenseData, message: 'Data added successfully' })
+    }
+    catch (err) {
+        console.log('post expense err = ' + err);
+        return res.status(500).json({ error: err })
+    }
+}
 
-router.delete('/delete-expense/:id', userAuthorization.authenticate, expenseController.deleteExpense);
+exports.deleteExpense = async (req, res, next) => {
+    //post always uses body
+    const expenseID = req.params.id;
+    console.log('expenseID = ' + expenseID);
+    if (expenseID == '' || expenseID == undefined) {
+        return res.status(400).json({ message: 'Id is empty' })
+    }
 
+    const deleteExpenseData = await Expense.destroy({ where: { id: expenseID, userid: req.user.id } });
 
-module.exports = router;
+    res.status(200).json({ message: 'Data deleted successfully' })
+}
+
+exports.getExpense = async (req, res, next) => {
+
+    try {
+        const allExpenseData = await Expense.findAll({ where: { userid: req.user.id } });
+        res.status(200).json({ expenseData: allExpenseData });
+    } catch (err) {
+        console.log('getExpense err = ' + err);
+        return res.status(400).json({ error: err })
+    }
+}
--- premium.js
+++ premium.js
@@ -1,61 +1,14 @@
 
 const User = require('../models/user');
-const Expense = require('../models/expense');
-const sequelize = require('sequelize');
-
 
 exports.getPremiumLB = async (req, res, next) => {
     try {
 
-        // const users = await User.findAll({
-        //     attributes: ['id', 'name']
-        // });
-        // console.log('getPremiumLB users = ' + JSON.stringify(users));
-        // const expenses = await Expense.findAll({
-        //     //sum of exenses for each id and group by userid
-        //     attributes: ['userid', [sequelize.fn('sum', sequelize.col('money')), 'total_cost']],
-        //     group:['userid']
-        // });
-        // console.log('getPremiumLB expenses = ' + JSON.stringify(expenses));
-       
-        // var userLeaderBoardDetails = [];
-        // users.forEach(user => {
-        //     userLeaderBoardDetails.push({ name: user.name, total_cost: userAggreagatedExpenses[user.id] })
-        // })
-        // Sort userLeaderBoardDetails based on total_cost in descending order
-        // userLeaderBoardDetails.sort((a, b) => b.total_cost - a.total_cost);
-
-        // console.log('userLeaderBoardDetails = ' + JSON.stringify(userLeaderBoardDetails));
-        // res.status(200).json(expenses);
-
-
-
-        const users = await User.findAll({
-            attributes: ['id','name']
+        const LBusers = await User.findAll({
+            order: [['totalExpenses', 'DESC']]
         });
-        console.log('getPremiumLB users = ' + JSON.stringify(users));
-        const expenses = await Expense.findAll({
-            attributes: ['userid', 'money']
-        });
-        console.log('getPremiumLB expenses = ' + JSON.stringify(expenses));
-        const userAggreagatedExpenses = {};
-        console.log('expenses = ' + JSON.stringify(expenses));
-        expenses.forEach(expense => {
-            if (userAggreagatedExpenses[expense.userid]) {
-                userAggreagatedExpenses[expense.userid] = userAggreagatedExpenses[expense.userid] + expense.money;
-            } else {
-                userAggreagatedExpenses[expense.userid] = expense.money;
-            }
-        })
-        var userLeaderBoardDetails = [];
-        users.forEach(user => {
-            userLeaderBoardDetails.push({ name: user.name, total_cost: userAggreagatedExpenses[user.id] })
-        })
-        // Sort userLeaderBoardDetails based on total_cost in descending order
-        userLeaderBoardDetails.sort((a, b) => b.total_cost - a.total_cost);
-
-        console.log('userLeaderBoardDetails = ' + JSON.stringify(userLeaderBoardDetails));
-        res.status(200).json(userLeaderBoardDetails);
+        console.log('getPremiumLB LBusers = ' + JSON.stringify(LBusers));
+        res.status(200).json(LBusers);
 
     }//try
     catch (err) {

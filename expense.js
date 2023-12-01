058a867881f7ba261d9f5f8bac70001f3b3cb0d0
Pushkar Sabe <83827883+pushkarsabe@users.noreply.github.com>
2023-12-01 22:32:33 +0530

    Question 13 = Making Expense App Production ready

--- expense.js
+++ expense.js
@@ -1,7 +1,11 @@
 const Expense = require('../models/expense');
 const User = require('../models/user');
+const sequelize = require('../util/database')
 
 exports.postAddExpense = async (req, res, next) => {
+    //any post request if faces any problem but gets saved in database then transaction is used and keeps track of CRUD operations and if anything goes wrong it will roll back and if nothing happens it will commmit
+    const t = await sequelize.transaction();
+
     //post always uses body
     try {
         const money = req.body.money;
@@ -20,7 +24,7 @@ exports.postAddExpense = async (req, res, next) => {
             description: description,
             options: options,
             userid: req.user.id
-        });
+        }, { transanction: t });
         //to keep the total money updated spend by the user every time new expense is added 
         const totalExpense = Number(req.user.totalExpenses) + Number(money);
         console.log('req.user objet= ' + JSON.stringify(req.user));
@@ -31,33 +35,63 @@ exports.postAddExpense = async (req, res, next) => {
             totalExpenses: totalExpense
         },
             {
-                where: {
-                    id: req.user.id
-                },
-
+                where: { id: req.user.id },
+                transanction: t
             });
 
         console.log('Updated rows count:', updatedRowsCount);
-
+        //successfully save the chnages to the database
+        await t.commit();
         res.status(200).json({ newExpenseData: expenseData, message: 'Data added successfully' })
     }
     catch (err) {
+        //do not save the chnages to the database
+        await t.rollback();
         console.log('post expense err = ' + err);
         return res.status(500).json({ error: err })
     }
 }
 
 exports.deleteExpense = async (req, res, next) => {
-    //post always uses body
-    const expenseID = req.params.id;
-    console.log('expenseID = ' + expenseID);
-    if (expenseID == '' || expenseID == undefined) {
-        return res.status(400).json({ message: 'Id is empty' })
+    const t = await sequelize.transaction();
+
+    try {
+        //post always uses body
+        const t = await sequelize.transaction();
+
+        const expenseID = req.params.id;
+        console.log('expenseID = ' + expenseID);
+        if (expenseID == '' || expenseID == undefined) {
+            return res.status(400).json({ message: 'Id is empty' })
+        }
+
+        // console.log('deleteExpense req.user object = ' + JSON.stringify(req.user));
+        const expenseMoneyOFParticularUserid = await Expense.findAll({ where: { id: expenseID } });
+        console.log('deleteExpense expenseMoneyOFParticularUserid = ' + JSON.stringify(expenseMoneyOFParticularUserid[0].money));
+        //this will reduce the amount money from totalexpense column
+        const totalExpense = Number(req.user.totalExpenses) - expenseMoneyOFParticularUserid[0].money;
+        console.log('deleteExpense totalExpense = ' + totalExpense);
+        //this will update the totalExpenses column in user table
+        const [updatedRowsCount] = await User.update({
+            totalExpenses: totalExpense
+        },
+            {
+                where: { id: req.user.id },
+                transanction: t
+            });
+
+        console.log('deleteExpense Updated rows count:', updatedRowsCount);
+        const deleteExpenseData = await Expense.destroy({ where: { id: expenseID, userid: req.user.id }, transanction: t });
+        await t.commit();
+
+        res.status(200).json({ message: 'Data deleted successfully' })
     }
-
-    const deleteExpenseData = await Expense.destroy({ where: { id: expenseID, userid: req.user.id } });
-
-    res.status(200).json({ message: 'Data deleted successfully' })
+    catch (err) {
+        //do not save the chnages to the database
+        await t.rollback();
+        console.log('deleteExpense err = ' + err);
+        return res.status(400).json({ error: err })
+    }
 }
 
 exports.getExpense = async (req, res, next) => {

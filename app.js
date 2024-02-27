b5ec7e9902b64a762aaae6495c23d13d4b0f61a6
Pushkar Sabe <83827883+pushkarsabe@users.noreply.github.com>
2024-02-27 20:27:41 +0530

    Question 23 = Lets get started with typescript

--- app.js
+++ app.js
@@ -1,90 +1,43 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-//to avoid cross origin comm error
-const cors = require('cors');
-//to secure requests 
-const helmet = require('helmet');
-//compression generally refers to the process of compressing data or files
-const compression = require('compression');
-//HTTP request logger middleware for node.js ,it provides a simple and configurable way to log HTTP requests and responses in your server application. 
-const morgan = require('morgan'); 
-const fs = require('fs');
-const path = require('path');
-require('dotenv').config();
-
-const sequelize = require('./util/database');
-
-//to write all the logs inside one file 
-const accessLogStream = fs.createWriteStream(
-  path.join(__dirname, 'access.log'),
-  { flags: 'a' }
-)
-
-const app = express();
-app.use(cors());
-app.use(helmet());
-app.use(compression());
-app.use(morgan('combined', { stream: accessLogStream }));
-
-const userRoute = require('./routes/user');
-const expenseRoute = require('./routes/expense');
-const purchaseRoute = require('./routes/purchase');
-const premiumRoute = require('./routes/premium');
-const forgotpasswordRoute = require('./routes/forgotpassword');
-
-const User = require('./models/user');
-const Order = require('./models/orders');
-const ForgotPassword = require('./models/ForgotPasswordRequests');
-const DownloadExpense = require('./models/downloadExpense');
-
-//parse application
-app.use(bodyParser.urlencoded({ extended: false }));
-//parse application/json
-app.use(bodyParser.json());
-
-//to call user 
-app.use('/user', userRoute);
-
-//to call expense 
-app.use('/expense', expenseRoute);
-
-//to call purchase 
-app.use('/purchase', purchaseRoute);
-
-//to call premium 
-app.use('/premium', premiumRoute);
-
-//to call forgot password 
-app.use('/password', forgotpasswordRoute);
-
-//to show the FK relationship
-User.hasMany(Order);
-Order.belongsTo(User);
-
-User.hasMany(ForgotPassword);
-ForgotPassword.belongsTo(User);
-
-User.hasMany(DownloadExpense);
-DownloadExpense.belongsTo(User);
-
-//this will update the new column added into model into user table
-// User.sync({ alter: true })
-//   .then(() => {
-//     console.log('Model synced successfully');
-//   })
-//   .catch((error) => {
-//     console.error('Error syncing model:', error);
-//   });
-
-// this will create tabble
-sequelize
-  .sync()
-  .then(result => {
-    // console.log(result);
-    app.listen(process.env.PORT || 3000);
-  })
-  .catch(err => {
-    console.log(err);
-  });
-
-
+"use strict";
+const num1Element = document.getElementById("num1");
+const num2Element = document.getElementById("num2");
+const button = document.querySelector("button");
+const numResults = [];
+const textResults = [];
+function addNum(num1, num2) {
+    if (typeof num1 === "number" && typeof num2 === "number") {
+        return num1 + num2;
+    }
+    else if (typeof num1 === "string" && typeof num2 === "string") {
+        return num1 + " " + num2;
+    }
+    //   forced conversion to integer from string if one value is string
+    return +num1 + +num2;
+}
+function printResult(resultObj) {
+    console.log("resultObj = " + resultObj.val);
+}
+if (button) {
+    button.addEventListener("click", () => {
+        const num1 = num1Element.value;
+        const num2 = num2Element.value;
+        const integerValue = addNum(+num1, +num2);
+        numResults.push(integerValue);
+        console.log("result = " + integerValue);
+        const stringValue = addNum(num1, num2);
+        textResults.push(stringValue);
+        console.log("stringValue = " + stringValue);
+        printResult({ val: integerValue, timestamp: new Date() });
+        console.log("numResults = ", numResults + " textResults = ", textResults);
+    });
+}
+const myPromise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve("It worked!!");
+    }, 1000);
+});
+myPromise.then((result) => {
+    console.log("result = " + result.split("w"));
+});
+// console.log(addNum(1, 2));
+// console.log(addNum("5", "7"));//error

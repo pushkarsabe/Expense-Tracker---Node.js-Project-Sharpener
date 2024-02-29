b2637ace77d324925bc8eccde53e46c92ab8d25e
Pushkar Sabe <83827883+pushkarsabe@users.noreply.github.com>
2024-02-29 11:35:06 +0530

    Question 24 =  Integrating Typescript with Node and Express

--- app.js
+++ app.js
@@ -1,43 +1,12 @@
 "use strict";
-const num1Element = document.getElementById("num1");
-const num2Element = document.getElementById("num2");
-const button = document.querySelector("button");
-const numResults = [];
-const textResults = [];
-function addNum(num1, num2) {
-    if (typeof num1 === "number" && typeof num2 === "number") {
-        return num1 + num2;
-    }
-    else if (typeof num1 === "string" && typeof num2 === "string") {
-        return num1 + " " + num2;
-    }
-    //   forced conversion to integer from string if one value is string
-    return +num1 + +num2;
-}
-function printResult(resultObj) {
-    console.log("resultObj = " + resultObj.val);
-}
-if (button) {
-    button.addEventListener("click", () => {
-        const num1 = num1Element.value;
-        const num2 = num2Element.value;
-        const integerValue = addNum(+num1, +num2);
-        numResults.push(integerValue);
-        console.log("result = " + integerValue);
-        const stringValue = addNum(num1, num2);
-        textResults.push(stringValue);
-        console.log("stringValue = " + stringValue);
-        printResult({ val: integerValue, timestamp: new Date() });
-        console.log("numResults = ", numResults + " textResults = ", textResults);
-    });
-}
-const myPromise = new Promise((resolve, reject) => {
-    setTimeout(() => {
-        resolve("It worked!!");
-    }, 1000);
-});
-myPromise.then((result) => {
-    console.log("result = " + result.split("w"));
-});
-// console.log(addNum(1, 2));
-// console.log(addNum("5", "7"));//error
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const body_parser_1 = __importDefault(require("body-parser"));
+const todos_1 = __importDefault(require("./routes/todos"));
+const app = (0, express_1.default)();
+app.use(body_parser_1.default.json());
+app.use(todos_1.default);
+app.listen({ port: 3000 });
--- app.ts
+++ app.ts
@@ -1,57 +1,10 @@
-const num1Element = document.getElementById("num1") as HTMLInputElement;
-const num2Element = document.getElementById("num2") as HTMLInputElement;
-const button = document.querySelector("button")!;
+import express from "express";
+import bodyParser from "body-parser";
+import todosRoutes from "./routes/todos";
 
-const numResults: Array<number> = [];
-const textResults: string[] = [];
+const app = express();
+app.use(bodyParser.json());
 
-type NumOrString = number | string;
-type Result = { val: number; timestamp: Date };
+app.use(todosRoutes);
 
-interface ResultObj {
-  val: number;
-  timestamp: Date;
-}
-
-function addNum(num1: NumOrString, num2: NumOrString) {
-  if (typeof num1 === "number" && typeof num2 === "number") {
-    return num1 + num2;
-  } else if (typeof num1 === "string" && typeof num2 === "string") {
-    return num1 + " " + num2;
-  }
-  //   forced conversion to integer from string if one value is string
-  return +num1 + +num2;
-}
-
-function printResult(resultObj: Result) {
-  console.log("resultObj = " + resultObj.val);
-}
-
-if (button) {
-  button.addEventListener("click", () => {
-    const num1 = num1Element.value;
-    const num2 = num2Element.value;
-    const integerValue = addNum(+num1, +num2);
-    numResults.push(integerValue as number);
-    console.log("result = " + integerValue);
-
-    const stringValue = addNum(num1, num2);
-    textResults.push(stringValue as string);
-    console.log("stringValue = " + stringValue);
-    printResult({ val: integerValue as number, timestamp: new Date() });
-    console.log("numResults = ", numResults + " textResults = ", textResults);
-  });
-}
-
-const myPromise = new Promise<string>((resolve, reject) => {
-  setTimeout(() => {
-    resolve("It worked!!");
-  }, 1000);
-});
-
-myPromise.then((result) => {
-  console.log("result = " + result.split("w"));
-});
-
-// console.log(addNum(1, 2));
-// console.log(addNum("5", "7"));//error
+app.listen({ port: 3000 });
